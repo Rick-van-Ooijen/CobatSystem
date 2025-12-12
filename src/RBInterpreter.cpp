10cbec27aa70c812890a42c7661d84de9bbe00f7
Rick van Ooijen <211295@buas.nl>
2025-12-12 16:07:47 +0100

    Revert "string literals"
    This reverts commit 15b12c45d0a85e733ba98560e2141786fd5af85e.

--- src/RBInterpreter.cpp
+++ src/RBInterpreter.cpp
@@ -141,14 +141,8 @@ std::vector<Token> Scanner::scanTokens(std::string source)
 			{
 				line++;
 			} break;
-			case '"': {string();} break;
 			default:
 			{
-				if ()
-				{
-					
-				}
-
 				interpreter->reportError(line, "Unexpected character.");
 			}
 		}
@@ -166,27 +160,4 @@ void Scanner::addToken(int type, std::string literal)
 	std::string text = source.substr(start, (current - start));
 
 	tokens.push_back(Token(type, text, literal, line));
-}
-
-void Scanner::string()
-{
-	while (source[current] != '"' && !(current >= source.length())) 
-	{
-		if (char(source[current]) == '\n')
-		{
-			line++;
-		}
-		current++;
-	}
-
-	if(current >= source.length())
-	{
-		interpreter->reportError(line, "Unterminated string.");
-	}
-
-	
-	std::string value = source.substr(start + 1, current - 1);
-	current++;
-	addToken(TokenType::T_STRING, value);
-	
 }
